44b5272f7fb376ec6c904bf5682e811546ad462f
Sriikk <145980751+SriAndayani@users.noreply.github.com>
2025-07-04 09:21:40 +0800

    Add files via upload

---
+++ UAS_wapad/src/uas_wapad/komponen/AutoCompleteComboBox.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uas_wapad.komponen;
+
+import javax.swing.JComboBox;
+
+/**
+ *
+ * @author Sri Andayani
+ */
+//public class AutoCompleteComboBox extends JComboBox<MenuItem> {
+//    private final List<MenuItem> items;
+//
+//    public AutoCompleteComboBox(List<MenuItem> items) {
+//        super(new DefaultComboBoxModel<>(items.toArray(new MenuItem[0])));
+//        this.items = items;
+//        setEditable(true);
+//        JTextField tf = (JTextField) getEditor().getEditorComponent();
+//        tf.addKeyListener(new KeyAdapter() {
+//            public void keyReleased(KeyEvent ke) {
+//                String input = tf.getText();
+//                DefaultComboBoxModel<MenuItem> m = (DefaultComboBoxModel<MenuItem>) getModel();
+//                m.removeAllElements();
+//                for (MenuItem item : items) {
+//                    if (item.getName().toLowerCase().startsWith(input.toLowerCase())) {
+//                        m.addElement(item);
+//                    }
+//                }
+//                setPopupVisible(m.getSize() > 0);
+//                tf.setText(input);
+//            }
+//        });
+//    }
+//}
+
